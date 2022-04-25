4ada3a3233a98c9684948f9d2a95006f8b68414f
Archana Kalathiya <archanakalathiya91@gmail.com>
2022-04-25 14:17:10 +0530

    Program 3

---
+++ Shell Programs/S3.sh
@@ -0,0 +1,9 @@
+# Shell Script using While loop 
+# The shell Program for printing the first positive numbers using while loop 
+n=1
+while [ $n -le 10 ]
+do
+	echo $ctr
+	num=`expr $ctr + 1`  #increment num by 1
+done 
+
