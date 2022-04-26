b2e207d481554ead88cd4a4983d75df30aa90f99
Archana Kalathiya <archanakalathiya91@gmail.com>
2022-04-26 19:10:40 +0530

    User commands

---
+++ Shell Programs/S13.sh
@@ -0,0 +1,8 @@
+who # It displays the login name , terminal time , login time  , remote hostname (to show the lists of the users currently logged into the system)
+who -H #To display the column heading
+who -m #to show hostname and user associated with stdin
+who -l #To display the system log in processes
+who -q #To display all login names and number of users logged in 
+who -r #To display the current system runlevel type 
+who -r -b 
+who am i
