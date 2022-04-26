a17063ed9a1e563cd314ffb38d13e4889e60842e
Archana Kalathiya <archanakalathiya91@gmail.com>
2022-04-26 16:26:36 +0530

    Reaadble or Writable file

--- Shell Programs/S9.sh
+++ Shell Programs/S9.sh
@@ -7,3 +7,17 @@ echo "$fn is readable "
 else 
 echo "$fn is not readable"
 fi
+#Check if file is writable or not 
+if [ -w "$fn" ];
+then 
+echo "$fn is writable "
+else 
+echo "$fn is not writable"
+fi
+#Check if file is both readable and writable
+if [ -r "$fn" -a -w "$fn" ];
+then
+echo "$fn is both readable and writable "
+else 
+echo "$fn is not both readable and writable"
+fi
